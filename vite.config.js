a4ffc6bc6c9714576021313231980f8afae6ce38
unknown <syedzubaira978@gmail.com>
2025-04-30 12:30:47 +0530

    Fix: Added Vite and build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
-  base: process.env.VITE_BASE_PATH || "/Portfolio-app"
 })
